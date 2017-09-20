50c751ff6842569f16a9bf5ec90a3c09a30ed071
David Zavaleta <davidczavaleta@gmail.com>
2017-09-19 20:52:53 -0500

    Integrated skills via model generator

---
+++ app/models/skill.rb
@@ -0,0 +1,9 @@
+class Skill < ApplicationRecord
+end
+
+ #reate_table "skills", force: :cascade do |t|
+ # t.string   "title"
+ # t.integer  "percent_utilized"
+ # t.datetime "created_at",       null: false
+ # t.datetime "updated_at",       null: false
+ #end
