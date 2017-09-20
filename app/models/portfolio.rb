3af683c347b5985522b70f60b7ffde2291ebdbcf
David Zavaleta <davidczavaleta@gmail.com>
2017-09-19 21:08:10 -0500

    Added portfolio items via resource generator

---
+++ app/models/portfolio.rb
@@ -0,0 +1,12 @@
+class Portfolio < ApplicationRecord
+end
+
+#create_table "portfolios", force: :cascade do |t|
+#t.string   "title"
+#t.string   "subtitle"
+#  t.text     "body"
+#  t.text     "main_image"
+#  t.text     "thumb_image"
+#  t.datetime "created_at",  null: false
+#  t.datetime "updated_at",  null: false
+#end
